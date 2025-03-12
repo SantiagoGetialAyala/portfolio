bd086d4c62c66ab9e9a523aa3c64b7cb0d4c128a
SantiagoGetialAyala <brayan.getial@campusucc.edu.co>
2025-03-12 07:51:02 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center w-screen h-screen bg-blue-200">
+      <h1 className="font-bold text-4xl">Santiago Getial Ayala</h1>
+    </main>
+
+  );
+}
