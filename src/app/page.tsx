71cc9ff7b2f78a16ba6e12fab9294da6feee4fff
SantiagoGetialAyala <brayan.getial@campusucc.edu.co>
2025-03-14 14:57:19 -0500

    fix: updated color palette with hsl and adjusted global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center w-screen h-screen bg-blue-200">
+    <main className="flex justify-center items-center w-screen h-screen bg-secondary">
       <h1 className="font-bold text-4xl">Santiago Getial Ayala</h1>
     </main>
 
